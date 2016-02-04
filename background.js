3cd6bbe83ea80a1e280793065d67441a371d609d
jim60105 <jim60105@gmail.com>
2016-02-05 00:54:07 +0800

    新增功能: 1.check失敗JOB
    網址使用: batchremote/batch.html

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+//確認網址
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
 function checkForValidUrl(tabId, changeInfo, tab) {
     if(getDomainFromUrl(tab.url).toLowerCase()=="batchremote.jim60105.com"){
@@ -15,3 +16,21 @@ function getDomainFromUrl(url){
         host = match[1];
     return host;
 }
+
+//儲存errorList
+var errorList=[];
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.addErrName != undefined)
+            errorList[errorList.length] = request.addErrName;
+        if (request.chkErrName != undefined) {
+            var state = false;
+            for (var i in errorList) {
+                if (errorList[i] == request.chkErrName) {
+                    state = true;
+                }
+            }
+            sendResponse({ignore: state});
+        }
+    }
+);
--- content_script.js
+++ content_script.js
@@ -3,8 +3,16 @@ var batchAmount;
 batchAmount = $("form:last").get(0).name.match(/\d+/)[0];
 
 //添加按鈕
-var newItem = $('<div><input type="button" value="隱藏成功項" id="butHideSuccess"/></div>').click(hideSuccess);
-$("body").prepend(newItem);
+var newBar = $('<div style="width: 100%" id="bar"></div>')
+var butHideSuccess = $('<input type="button" value="隱藏成功項" id="butHideSuccess"/>').click(hideSuccess);
+var butCheckError = $('<input type="button" value="檢查失敗項" id="butCheckError"/>').click(errorCheck);
+$("body").prepend(newBar);
+$("#bar").prepend(butCheckError);
+$("#bar").prepend(butHideSuccess);
+
+//每分鐘重新整理+checkError
+//setTimeout(function(){ errorCheck(window.location.reload()); }, 60000);
+
 
 //隱藏/還原已成功項
 function hideSuccess(){
@@ -22,5 +30,32 @@ function hideSuccess(){
             }
         }
         $("#butHideSuccess").get(0).value = "還原成功項";
+    }
+}
+var number = 0;
+
+//check失敗狀況
+function errorCheck(callback){
+    if ($("#td" + number + "5").get(0).innerHTML.indexOf("失敗") > 0) {
+        var jobName = $("#td" + number + "1").get(0).innerHTML;
+        chrome.runtime.sendMessage({chkErrName: jobName}, function(response) {
+            if(!response.ignore){
+                alert(jobName+" 執行失敗!!!!!");
+                chrome.runtime.sendMessage({addErrName: jobName}, function(response) {
+                });
+            }else{
+                console.log(jobName+" 已check");
+            }
+            number++;
+            if(number<batchAmount)
+                errorCheck(callback);
+        });
+    }else{
+        number++;
+        if(number<batchAmount) {
+            errorCheck(callback);
+        }else{
+            number=0;
+        }
     }
 }
